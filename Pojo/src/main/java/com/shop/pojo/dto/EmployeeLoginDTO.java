0dc81f93416629acfc3c5720ad089e688438de36
SpadeKTLSG <spadekxcwxtlsg@gmail.com>
2024-06-01 09:28:57 +0800

    CRUD 2 管理员 1

--- Pojo/src/main/java/com/shop/pojo/dto/EmployeeLoginDTO.java
+++ Pojo/src/main/java/com/shop/pojo/dto/EmployeeLoginDTO.java
@@ -4,4 +4,14 @@ import lombok.Data;
 
 @Data
 public class EmployeeLoginDTO {
+
+    /**
+     * 账号
+     */
+    private String account;
+
+    /**
+     * 密码
+     */
+    private String password;
 }
